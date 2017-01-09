40c252c5b640de9aedcafa897153d9cde6afe60a
Pedro S. Moreira <pedsmoreira@gmail.com>
2017-01-09 01:28:44 -0200

    Fix cors typo

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'premiere-player.herokuapp.com/'
+    origins 'premiere-player.herokuapp.com'
 
     resource '*',
       headers: :any,
